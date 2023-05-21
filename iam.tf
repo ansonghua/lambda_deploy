a2c6424f073967b8c39616a7f9d687e80e3bfd85
Amy An <ansonghua@hotmail.com>
2023-05-21 17:01:10 -0500

    uncomment

--- iam.tf
+++ iam.tf
@@ -1,69 +1,69 @@
-# resource "aws_iam_role" "lambda_exec_role" {
-#   name = "lambda_exec_role_2"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "lambda.amazonaws.com"
-#       }
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role_2"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
 
-# data "aws_iam_policy_document" "lambda_policy_doc" {
-#   statement {
-#     sid = "AllowInvokingLambdas"
-#     effect = "Allow"
+data "aws_iam_policy_document" "lambda_policy_doc" {
+  statement {
+    sid = "AllowInvokingLambdas"
+    effect = "Allow"
 
-#     resources = [
-#       "arn:aws:lambda:*:*:function:*"
-#     ]
+    resources = [
+      "arn:aws:lambda:*:*:function:*"
+    ]
 
-#     actions = [
-#       "lambda:InvokeFunction"
-#     ]
-#   }
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+  }
 
-#   statement {
-#     sid = "AllowCreatingLogGroups"
-#     effect = "Allow"
+  statement {
+    sid = "AllowCreatingLogGroups"
+    effect = "Allow"
 
-#     resources = [
-#       "arn:aws:logs:*:*:*"
-#     ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
 
-#     actions = [
-#       "logs:CreateLogGroup"
-#     ]
-#   }
+    actions = [
+      "logs:CreateLogGroup"
+    ]
+  }
 
-#   statement {
-#     sid = "AllowWritingLogs"
-#     effect = "Allow"
+  statement {
+    sid = "AllowWritingLogs"
+    effect = "Allow"
 
-#     resources = [
-#       "arn:aws:logs:*:*:log-group:/aws/lambda/*:*"
-#     ]
+    resources = [
+      "arn:aws:logs:*:*:log-group:/aws/lambda/*:*"
+    ]
 
-#     actions = [
-#       "logs:CreateLogStream",
-#       "logs:PutLogEvents",
-#     ]
-#   }
-# }
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+}
 
-# resource "aws_iam_policy" "lambda_iam_policy" {
-#   name = "lambda_iam_policy_2"
-#   policy = data.aws_iam_policy_document.lambda_policy_doc.json
-# }
+resource "aws_iam_policy" "lambda_iam_policy" {
+  name = "lambda_iam_policy_2"
+  policy = data.aws_iam_policy_document.lambda_policy_doc.json
+}
 
-# resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-#   policy_arn = aws_iam_policy.lambda_iam_policy.arn
-#   role = aws_iam_role.lambda_exec_role.name
-# }
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = aws_iam_policy.lambda_iam_policy.arn
+  role = aws_iam_role.lambda_exec_role.name
+}
