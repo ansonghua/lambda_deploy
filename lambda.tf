c205accfaf574dfa21b080cd5ec8caa2ea5c0a85
Amy An <ansonghua@hotmail.com>
2023-05-27 16:48:57 -0500

    init

--- lambda.tf
+++ lambda.tf
@@ -1,64 +1,27 @@
-# resource "null_resource" "install_python_dependencies" {
-#   provisioner "local-exec" {
-#     command = "bash ${path.module}/scripts/create_pkg.sh"
+data "archive_file" "test_zip" {
+  type        = "zip"
+  source_dir = "${path.module}/lambda_function"
+  output_path = "${path.module}/myzip/test.zip"
+}
 
-#     environment = {
-#       source_code_path = var.path_source_code
-#       function_name = var.function_name
-#       path_module = path.module
-#       runtime = var.runtime
-#       path_cwd = path.cwd
-#     }
-#   }
-# }
-
-# resource "null_resource" "install_dependencies" {
-#   provisioner "local-exec" {
-#     command = "pip install -r ${var.lambda_root}/requirements.txt -t ${var.lambda_root}/"
-#   }
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "${path.module}/myzip/test.zip"
+  source_code_hash = data.archive_file.test_zip.output_base64sha256
+  function_name = "test_function"
+  role          = aws_iam_role.insights_lambda_role.arn
+  handler       = "test_function.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 600
+  memory_size   = 192
   
-#   triggers = {
-#     dependencies_versions = filemd5("${var.lambda_root}/requirements.txt")
-#     source_versions = filemd5("${var.lambda_root}/function.py")
-#   }
-# }
-# data "archive_file" "create_dist_pkg" {
-#   depends_on = [null_resource.install_python_dependencies]
-#   source_dir = "${path.cwd}/lambda_dist_pkg/"
-#   output_path = var.output_path
-#   type = "zip"
-# }
+  layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:3"]
 
-# resource "aws_lambda_function" "aws_lambda_test" {
-#   depends_on = [null_resource.install_python_dependencies]
-#   function_name = var.function_name
-#   description = "Process video and does face recognition..."
-#   handler = "lambda_function.lambda.lambda_handler"
-#   runtime = var.runtime
-
-#   role = aws_iam_role.lambda_exec_role.arn
-#   memory_size = 128
-#   timeout = 300
-
-#   source_code_hash = data.archive_file.create_dist_pkg.output_base64sha256
-#   filename = data.archive_file.create_dist_pkg.output_path
-# }
-
-resource "aws_lambda_function" "aws_lambda_test" {
-  s3_bucket = "amy-lambda-deployment"
-  s3_key    = "lambda.zip"
-  function_name    = "test-lambda"
-  handler          = "module.handler"
-  runtime          = "python3.9"
-  timeout          = 180
-  role = aws_iam_role.lambda_exec_role.arn
-  memory_size = 128
-}
-
-resource "aws_lambda_permission" "allow_bucket" {
-  function_name = aws_lambda_function.aws_lambda_test.arn
-  source_arn = aws_s3_bucket.bucket_read_videos.arn
-  statement_id = "AllowExecutionFromS3Bucket"
-  action = "lambda:InvokeFunction"
-  principal = "s3.amazonaws.com"
-}
+  environment {
+    variables = {
+      aws_region = var.region
+      EMAIL_TO       = var.email_to
+      EMAIL_FROM       = var.email_from
+      CONFIG_SECRET_ARN       = aws_secretsmanager_secret.my_secret.arn
+      BUCKET_NAME             = aws_s3_bucket.my_bucket.bucket
+    }
+  }
--- variables.tf
+++ variables.tf
@@ -1,26 +1,42 @@
-variable "path_source_code" {
-  default = "lambda_function"
+variable "env" {
+  default = "dev"
 }
 
-variable "function_name" {
-  default = "aws_lambda_test"
+variable "region" {
+  default = "us-east-1"
 }
 
-variable "runtime" {
-  default = "python3.7"
+variable "email_to" {
+  default = "us-east-1"
 }
 
-variable "output_path" {
-  description = "Path to function's deployment package into local filesystem. eg: /path/lambda_function.zip"
-  default = "lambda_function.zip"
+variable "email_from" {
+  default = "us-east-1"
 }
 
-variable "distribution_pkg_folder" {
-  description = "Folder name to create distribution files..."
-  default = "lambda_dist_pkg"
-}
+# variable "path_source_code" {
+#   default = "lambda_function"
+# }
 
-variable "bucket_for_videos" {
-  description = "Bucket name for put videos to process..."
-  default = "aws-lambda-function-read-videos-amy"
-}
+# variable "function_name" {
+#   default = "aws_lambda_test"
+# }
+
+# variable "runtime" {
+#   default = "python3.7"
+# }
+
+# variable "output_path" {
+#   description = "Path to function's deployment package into local filesystem. eg: /path/lambda_function.zip"
+#   default = "lambda_function.zip"
+# }
+
+# variable "distribution_pkg_folder" {
+#   description = "Folder name to create distribution files..."
+#   default = "lambda_dist_pkg"
+# }
+
+# variable "bucket_for_videos" {
+#   description = "Bucket name for put videos to process..."
+#   default = "aws-lambda-function-read-videos-amy"
+# }
