287891a966bc5218f166ef9be59f41b937e49fa0
Amy An <ansonghua@hotmail.com>
2023-05-27 16:51:55 -0500

    init

--- lambda.tf
+++ lambda.tf
@@ -8,12 +8,12 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "${path.module}/myzip/test.zip"
   source_code_hash = data.archive_file.test_zip.output_base64sha256
   function_name = "test_function"
-  role          = aws_iam_role.insights_lambda_role.arn
+  role          = aws_iam_role.lambda_exec_role.arn
   handler       = "test_function.lambda_handler"
   runtime       = "python3.9"
   timeout       = 600
   memory_size   = 192
-  
+}
   layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:3"]
 
   environment {
