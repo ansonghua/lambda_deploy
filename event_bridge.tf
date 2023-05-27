595bb90aae2bf8616b6c3a285a19b237f9ea26e1
Amy An <ansonghua@hotmail.com>
2023-05-27 17:21:04 -0500

    init

---
+++ event_bridge.tf
@@ -0,0 +1,27 @@
+resource "aws_cloudwatch_event_rule" "s3_object_create_invoke_lambda" {
+
+  name           = "insightdb-justifications-rule"
+  description    = "Justification file has been posted"
+
+  event_pattern = <<EOF
+{
+  "source": ["aws.s3"],
+  "detail-type": ["Object Created"],
+  "detail": {
+    "bucket": {
+      "name": ["${aws_s3_bucket.my_bucket.bucket}"]
+    },
+    "object": {
+      "key": [{
+        "prefix": "justifications_processed"
+      }]
+    }
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "s3_object_create_invoke_lambda" {
+  rule           = aws_cloudwatch_event_rule.s3_object_create_invoke_lambda.name
+  arn            = aws_lambda_function.test_lambda.arn
+}
