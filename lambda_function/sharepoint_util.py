38c6167e406692bff1703a4ea55a792eb15e5ca8
Amy An <ansonghua@hotmail.com>
2023-05-28 11:59:05 -0500

    share point

--- lambda_function/sharepoint_util.py
+++ lambda_function/sharepoint_util.py
@@ -28,3 +28,17 @@ def get_access_token(tenant_id, app_id, client_secret):
     else:
         raise Exception("No Access Token found")
 
+
+def upload_file_to_sharepoint(access_token, upload_url, src_file_path):
+
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/octet-stream',
+        'Content-Length': str(os.path.getsize(src_file_path))
+    }
+    with open(src_file_path, 'rb') as file:
+        response = requests.put(upload_url, headers=headers, data=file)
+        print(response.json())
+
+def get_sharepoint_file_path(drive_path, file_relative_path):
+    return f'{drive_path}/{file_relative_path}:/content'
--- lambda_function/test_function.py
+++ lambda_function/test_function.py
@@ -2,7 +2,7 @@ import os
 import boto3
 import logging
 from utils import get_secret
-from sharepoint_util import get_access_token
+from sharepoint_util import get_access_token, upload_file_to_sharepoint,get_drive_id,get_sharepoint_file_path
 
 DEFAULT_TAGS = os.environ.get("DEFAULT_TAGS")
 print("DEFAULT_TAGS", DEFAULT_TAGS)
@@ -29,17 +29,32 @@ def lambda_handler(event, context):
     client_secret = sharepoint_secret['client_secret']
 
     scan_staus = 'Initial'
+    s3 = boto3.resource('s3')
     if scan_staus == 'Initial':
-        s3 = boto3.resource('s3')
         copy_source = {
             'Bucket': bucket_name,
             'Key': file_name
         }
         s3.meta.client.copy(copy_source, bucket_name, f'scan_result/{file_name_without_prefix}')
-        s3.Object(os.environ['REPROT_BUCKET_NAME'],file_name).delete()
 
+    
     access_token = get_access_token(tenant_id, app_id, client_secret)
+ 
+    client_drive_name = client_secret['client_drive_name']
+    client_site_id = client_secret['client_site_id']
+    site_url_prefix = f'https://graph.microsoft.com/v1.0/sites/{client_site_id}'
+    drive_id = get_drive_id(access_token,site_url_prefix, client_drive_name)
+    drive_path = f'{site_url_prefix}/drives/{drive_id}/root:'
+
+    folder_relative_path = 'Amy'
+    file_name = 'template.pptx'
+    file_relative_path = f'{folder_relative_path}/{file_name}'
+    sharepoint_file_path = get_sharepoint_file_path(drive_path,file_relative_path)
+ 
+    s3.Bucket(bucket_name).download_file(file_name, f'/tmp/${file_name_without_prefix}')
+    upload_file_to_sharepoint(access_token, sharepoint_file_path, f'/tmp/${file_name_without_prefix}')
     # print(access_token)
+    s3.Object(os.environ['REPROT_BUCKET_NAME'],file_name).delete()
 
     instances = ['i-09cafb1d617acfd93']
 
@@ -47,6 +62,7 @@ def lambda_handler(event, context):
         logger.warning('No instances available with this tags')
     else:
         if event['action'] == 'stop':
+
             ec2_client.start_instances(InstanceIds=instances)
             logger.info('Starting instances.')
         # elif event['action'] == 'start':
@@ -54,3 +70,4 @@ def lambda_handler(event, context):
         #     logger.info('Stopping instances.')
         else:
             logger.warning('No instances availables with this tags')
+
