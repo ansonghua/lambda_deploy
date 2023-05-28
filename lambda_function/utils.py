4c98d7a5c552d1d2ea943adf9c4d8db7279fb45f
Amy An <ansonghua@hotmail.com>
2023-05-28 10:23:11 -0500

    get secret

---
+++ lambda_function/utils.py
@@ -0,0 +1,30 @@
+import json
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret(secret_name):
+    result = ''
+    region = os.environ['aws_region']
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region,
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as cli_err:
+        print(f"Client Errors: {cli_err} ")
+    except Exception as exception:
+        print(f'Failed to get Secret from Secret Manager: {str(exception)}')
+        
+    else:
+        if 'SecretString' in get_secret_value_response:
+            result = get_secret_value_response['SecretString']
+
+    return json.loads(result)
