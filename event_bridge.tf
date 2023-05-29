736696a82a56dba3c82945562fe6fd90986652f7
Amy An <ansonghua@hotmail.com>
2023-05-29 16:04:32 -0500

    justifi

--- event_bridge.tf
+++ event_bridge.tf
@@ -72,6 +72,11 @@ resource "aws_cloudwatch_event_target" "s3_object_create_invoke_lambda" {
   arn            = aws_lambda_function.test_lambda.arn
 }
 
+resource "aws_cloudwatch_event_target" "s3_object_create_invoke_justification_lambda" {
+  rule           = aws_cloudwatch_event_rule.s3_object_create_invoke_jsutification_lambda.name
+  arn            = aws_lambda_function.justification_lambda.arn
+}
+
 resource "aws_cloudwatch_event_target" "s3_object_delete_invoke_lambda" {
   rule           = aws_cloudwatch_event_rule.s3_object_delete_invoke_lambda.name
   input          = "{\"action\":\"stop\"}"
